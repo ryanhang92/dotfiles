cf8ed76b93e5c521132cf259f34e8641a5540d17
Ryan Hang <ryanhang92@gmail.com>
2017-10-19 06:03:48 +0000

    updating dot files for the remote environment

--- install.sh
+++ install.sh
@@ -5,11 +5,20 @@
 #done
 
 echo "setting up Ryan's environment"
+
 source install/developer_tools_install.sh
 source install/science_package_install.sh
-source install/git_install.sh
-source install/vim_install.sh
-source install/tmux_install.sh
-source install/bash_install.sh
-source install/zsh_install.sh
-source install/emacs_install.sh
+
+chmod u+x install/git_install.sh
+chmod u+x install/vim_install.sh
+chmod u+x install/tmux_install.sh
+chmod u+x install/bash_install.sh
+chmod u+x install/zsh_install.sh
+chmod u+x install/emacs_install.sh
+
+./install/git_install.sh
+./install/vim_install.sh
+./install/tmux_install.sh
+./install/bash_install.sh
+./install/zsh_install.sh
+./install/emacs_install.sh
