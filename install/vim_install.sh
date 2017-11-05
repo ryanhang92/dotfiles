9d7f6fded6870a74dd20494ff7b83a08bafa5d80
Ryan Hang <ryanhang92@gmail.com>
2017-11-05 19:32:35 +0000

    adding linux check for ycm install script

--- install/vim_install.sh
+++ install/vim_install.sh
@@ -11,9 +11,11 @@ fi
 echo "adding ryan's vimrc"
 cp ~/dotfiles/vim/.vimrc ~/.vimrc
 
-echo "installing ycm plugin after updating vimrc with vundle configs"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev python3-dev
-eval ~/.vim/bundle/YouCompleteMe/.install.py --clang-completer
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo -e "\n Linux detected so installing ycm on linux box, use desktop ide for osx or windows"
+    echo "installing ycm plugin after updating vimrc with vundle configs"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    sudo apt-get install build-essential cmake
+    sudo apt-get install python-dev python3-dev
+    eval ~/.vim/bundle/YouCompleteMe/.install.py --clang-completer
 
