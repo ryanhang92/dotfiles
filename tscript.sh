e2ef14090a8f7f1a02f0153a4a4b6b74b4499a7a
Ryan Hang <ryanhang92@gmail.com>
2018-11-12 07:11:12 +0000

    change format and colors

--- tscript.sh
+++ tscript.sh
@@ -1,18 +1,15 @@
-
-# setup a work space called `work` with two windows
-# I can alias to my work directories
-
+# Create a name for the initial tmux session.
 session="work"
 
-# set up tmux
-tmux
+# Create a new session called work and split the window.
+tmux new -s work -d
+tmux split-window -h -p 75 -t $session
 
-# create a new tmux session
-tmux new-session $session 
+# Open vim in the second window and return to the first window.
+tmux select-pane -t 1
+tmux send-keys -t $session "vim" C-m
+tmux select-pane -t 0
 
-# Split pane 1 horizontal by %80
-tmux splitw -v -p 20
+# Attach the session to the running tmux instance.
+tmux attach -t $session
 
-# Select pane 1, set dir to api, run vim
-tmux selectp -t 1 
-tmux send-keys "vim" C-m 
