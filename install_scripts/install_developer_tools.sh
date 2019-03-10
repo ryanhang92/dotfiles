0946300db0ee81abf193442f3b07d68c865837da
Ubuntu <ubuntu@ip-172-31-17-56.us-west-2.compute.internal>
2019-03-10 09:54:18 +0000

    Adding init.vim and fixing install script

--- install_scripts/install_developer_tools.sh
+++ install_scripts/install_developer_tools.sh
@@ -56,10 +56,10 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 echo "\n Installing prezto (new install assumed)"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+#setopt EXTENDED_GLOB
+#for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+#  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+#done
 
 chsh -s /bin/zsh
 
